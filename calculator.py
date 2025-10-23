72d2fe442b48b45791076a5f9d1350cccc485746
Dr.K.Kalaivani <k.kalaivani@sru.edu.in>
2025-10-23 16:09:57 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,14 +1,5 @@
-# calculator.py
-
 def add(a, b):
-    """Add two numbers"""
     return a + b
 
 def subtract(a, b):
-    """Subtract two numbers"""
     return a - b
-
-# Try it out
-if __name__ == "__main__":
-    print("5 + 3 =", add(5, 3))
-    print("5 - 3 =", subtract(5, 3))
